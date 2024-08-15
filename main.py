c6df720c3429e9236a722aeffae85792663afb34
Phu Le <phu3262006@gmail.com>
2024-08-15 17:27:49 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,39 @@
+print("Welcome to your To-do list!")
+
+def addTask():
+    task = input("Add your task here: ")
+    todo_list.append(task)
+
+def removeTask():
+    listTask()
+    remove = int(input("The task you wanna remove (#): "))
+    todo_list.pop(remove-1)
+    print(f"Task #{remove} has been removed.")
+    
+def listTask():
+    print("\nHere are your tasks: ")
+    for i, val in enumerate(todo_list, start=1):
+        print(f"#{i}. {val}")
+
+todo_list = []
+run = True
+
+while run:
+    print("\nSelect the follow option: ")
+    print("------------------")
+    print("1. add task")
+    print("2. remove task")
+    print("3. list the tasks")
+    print("4. Quit")
+    option = int(input("Choice: "))
+
+    if option == 1:
+        addTask()
+    elif option == 2:
+        removeTask()
+    elif option == 3:
+        listTask()
+    elif option == 4:
+        run = False
+
+print("\nGoodbye!")
